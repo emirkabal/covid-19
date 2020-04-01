73cb52d200dc01129eea54fbbf9d5a718e6f451d
Emir Kabal <kabalemir@gmail.com>
2020-04-01 22:55:47 +0300

    👌 Intital Commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hoşgeldiniz!
+
+Bu proje [https://covid19.saglik.gov.tr/](https://covid19.saglik.gov.tr/) sitesine ulaşarak **COVID-19** virisünün istatistiklerini png dosyasına döker.
+
+Geliştirmekten çekinmeyin "Pull requests" sayfasını izliyor olacağım.
---
+++ index.js
@@ -0,0 +1,82 @@
+const cron = require('node-cron');
+const puppeteer = require('puppeteer');
+const express = require('express');
+const fs = require('fs');
+var app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: "OK"
+    });
+});
+
+
+app.get('/file/:file', async (req, res) => {
+
+    let file = req.params.file;
+    let images = fs.readdirSync('./images');
+    
+    if (!file || !images.includes(file.toLocaleLowerCase())) {
+        return res.json({
+            status: "error"
+        });
+    }
+
+
+    let image = Buffer.from(fs.readFileSync('./images/'+file), 'base64');
+
+    res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': image.length
+    });
+
+    res.end(image);
+
+})
+
+app.listen(3000);
+
+
+const update = async () => {
+    
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 });
+    
+    await page.goto('https://covid19.saglik.gov.tr/', { waitUntil: 'networkidle2' });
+    
+    await page.screenshot({
+        path: 'images/info-table.png',
+        clip: {x: 43, y: 232, width: 894, height: 498}
+    });
+    console.log('saved info table');
+
+    await page.screenshot({
+        path: 'images/statistics-table.png',
+        clip: {x: 983, y: 232, width: 894, height: 497}
+    });
+
+    console.log('saved statistics table');
+
+
+    browser.close();
+
+} 
+
+
+update();
+cron.schedule('0 20 * * *', async () => {
+  
+    update();
+
+
+}, {
+
+  scheduled: true,
+  timezone: "Europe/Istanbul"
+
+});
+
+
+
