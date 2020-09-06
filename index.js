85197509f231b3a3bba3ee550395f1ecf6085494
Emir Kabal <kabalemir@gmail.com>
2020-09-07 01:21:59 +0300

    Update

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const cron = require('node-cron');
 const puppeteer = require('puppeteer');
+const fetch = require('node-fetch').default;
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 var app = express();
 
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        status: "OK"
-    });
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.get('/covid-tr.json', async (req, res) => {
+
+
+    const data = await fetch('https://covid19.saglik.gov.tr/covid19api?getir=sondurum').then((res) => res.json()).catch(() => null);
+
+    try {
+        res.status(200).json(data[0]);
+    } catch (error) {
+        res.json({status: 500, message: 'covid19.saglik.gov.tr adresine erişilemedi.'});
+    }
+
 });
 
 
@@ -55,25 +68,27 @@ const update = async () => {
         ]
     });
     const page = await browser.newPage();
-    page.setViewport({ width: 1920, height: 1080 });
+    page.setViewport({ width: 1080, height: 969 });
     
-    await page.goto('https://covid19.saglik.gov.tr/', { waitUntil: 'networkidle2' });
-    
+    await page.goto('https://covid19.saglik.gov.tr/', { waitUntil: 'networkidle0' });
+    await page.evaluate(_ => {
+        window.scrollBy(0, 700);
+    });
     let date = new Date();
     let dateStr = `${pad(date.getDate())}-${pad(date.getMonth() + 1)}-${date.getFullYear()}`;
 
     await page.screenshot({
         path: `./images/info-table.png`,
-        clip: {x: 43, y: 232, width: 894, height: 480}
+        clip: {x: 40, y: 930, width: 990, height: 480}
     });
     console.log('saved info table');
 
-    await page.screenshot({
-        path: `./images/statistics-table.png`,
-        clip: {x: 983, y: 232, width: 894, height: 497}
-    });
+    // await page.screenshot({
+    //     path: `./images/statistics-table.png`,
+    //     clip: {x: 983, y: 232, width: 894, height: 497}
+    // });
 
-    console.log('saved statistics table');
+    // console.log('saved statistics table');
 
 
     browser.close();
@@ -83,10 +98,7 @@ const update = async () => {
 
 update();
 cron.schedule('0,5,10,15,30,35,45 19,20,21 * * *', async () => {
-  
     update();
-
-
 }, {
 
   scheduled: true,
