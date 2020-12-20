d7379fa770df2e908f96ab68ca0883282e9fc02f
Emir Kabal <kabalemir@gmail.com>
2020-12-20 20:37:51 +0300

    changelog: Image feature has removed.

--- index.js
+++ index.js
@@ -1,115 +1,41 @@
 const cron = require('node-cron');
-const puppeteer = require('puppeteer');
 const fetch = require('node-fetch').default;
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-var app = express();
 
+var app = express();
+var covidData = {};
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-app.get('/covid-tr.json', async (req, res) => {
-
-
-    const data = await fetch('https://covid19.saglik.gov.tr/covid19api?getir=sondurum').then((res) => res.json()).catch(() => null);
-
-    try {
-        res.status(200).json(data[0]);
-    } catch (error) {
-        res.json({status: 500, message: 'covid19.saglik.gov.tr adresine erişilemedi.'});
-    }
-
+app.get('/data.json', async (req, res) => {
+    res.json(covidData);
 });
-
-
-app.get('/file/:file', async (req, res) => {
-
-    let file = req.params.file;
-    let images = fs.readdirSync('./images');
-
-    if (!file || !images.includes(file.toLocaleLowerCase())) {
-        return res.json({
-            status: "error"
-        });
-    }
-
-
-    let image = Buffer.from(fs.readFileSync('./images/'+file), 'base64');
-
-    res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Length': image.length
-    });
-
-    res.end(image);
-
-})
-
-app.listen(3000, () => {
-    console.log('web application started on 3000 port.');
-});
-
 
 const update = async () => {
-
-    if (!fs.existsSync('./images')) {
-        console.log('created images folder.');
-        fs.mkdirSync('./images');
+    const data = await fetch('https://covid19.saglik.gov.tr/')
+        .then((res) => res.text()).catch(() => null);
+    //
+    if (!data) return;
+    try {
+        const jsonData = JSON.parse(data.split('var sondurumjson =')[1].split(';//')[0].trim());
+        covidData = jsonData[0];
+    } catch (error) {
+        console.error(error);
     }
-    
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-        ]
-    });
-    const page = await browser.newPage();
-    page.setViewport({ width: 1920, height: 1080 });
-    
-    await page.goto('https://covid19.saglik.gov.tr/', { waitUntil: 'networkidle0' });
-    await page.evaluate(_ => {
-        window.scrollBy(0, 600);
-    });
-    let date = new Date();
-    // let dateStr = `${pad(date.getDate())}-${pad(date.getMonth() + 1)}-${date.getFullYear()}`;
-
-    await page.screenshot({
-        path: `./images/info-table.png`,
-        clip: {x: 290, y: 1010, width: 1338, height: 610}
-    });
-    browser.close();
-    console.log('saved info table');
-
-    // await page.screenshot({
-    //     path: `./images/statistics-table.png`,
-    //     clip: {x: 983, y: 232, width: 894, height: 497}
-    // });
-
-    // console.log('saved statistics table');
-
-
-
 } 
-
 
 update();
 cron.schedule('0,5,10,15,30,35,45 19,20,21 * * *', async () => {
     update();
 }, {
-
   scheduled: true,
   timezone: "Europe/Istanbul"
-
 });
 
 
-function pad(num) {
-    if (num < 10) return `0${num}`;
-    else return num;
-}
-
-
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`❤ Application started on ${process.env.PORT || 3000}`);
+});
