5a55572f1fc82c854ef537544a0f245a7bf5258f
Emir Kabal <kabalemir@gmail.com>
2021-01-08 14:14:59 +0300

    added: historical.json

--- index.js
+++ index.js
@@ -6,26 +6,47 @@ const path = require('path');
 
 var app = express();
 var covidData = {};
+var covidDatabutHistorical = [];
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-app.get('/data.json', async (req, res) => {
-    res.json(covidData);
+app.get('/data.json', (req, res) => {
+    res.json(req.query.new && covidData[0] ? covidData[0] : covidData);
 });
 
-const update = async () => {
-    const data = await fetch('https://covid19.saglik.gov.tr/')
-        .then((res) => res.text()).catch(() => null);
-    //
-    if (!data) return;
+app.get('/historical.json', (req,res) => {
+    res.json(covidDatabutHistorical);
+});
+
+const getHtml = async (url) => {
+    return (await fetch(url)
+        .then((res) => res.text()).catch(() => null));
+}
+const jsonParser = (html, type="sondurumjson") => {
+    let jsonData;
     try {
-        const jsonData = JSON.parse(data.split('var sondurumjson =')[1].split(';//')[0].trim());
-        covidData = jsonData[0];
+        jsonData = JSON.parse(html.split(`var ${type} =`)[1].split(';//')[0].trim());
     } catch (error) {
         console.error(error);
     }
+    return jsonData;
+} 
+
+const update = async () => {
+    (async () => {
+        const data = await getHtml('https://covid19.saglik.gov.tr/');
+        if (!data) return;
+        const json = jsonParser(data);
+        if (json) covidData = json;
+    })();
+    (async () => {
+        const data = await getHtml('https://covid19.saglik.gov.tr/TR-66935/genel-koronavirus-tablosu.html');
+        if (!data) return;
+        const json = jsonParser(data, 'geneldurumjson');
+        if (json) covidDatabutHistorical = json;
+    })();
 } 
 
 update();
